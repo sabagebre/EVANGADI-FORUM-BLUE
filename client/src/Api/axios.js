1c604d54e4d646a705277d42eb23de0a236b919e
sabagebre <gebresaba467@gmail.com>
2025-04-03 22:13:51 -0600

    api updated

--- client/src/Api/axios.js
+++ client/src/Api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: "https://evangadi-forum-blue.onrender.com/api",
+  baseURL: "https://evangadi-forum-blue.onrender.com",
   // baseURL: "http://localhost:2017/api",
 });
 
